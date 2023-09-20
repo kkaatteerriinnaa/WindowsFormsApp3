9467b53f10b244d8b64bcfcdfad9ecaa1f71ec8c
kkaat <kkaat@KATERINA>
2023-09-20 16:08:16 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Name";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(25, 119);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(194, 189);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(87, 326);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(63, 90);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 11;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 358);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private readonly PersonPresenter _presenter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _presenter = new PersonPresenter(this, listView1); 
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public interface IPersonView
+        {
+            string Name { get; set; }
+            int Age { get; set; }
+
+            event EventHandler AddPerson;
+            event EventHandler SearchPerson;
+        }
+        public class PersonPresenter
+        {
+            private readonly IPersonView _view;
+            private readonly List<Person> _people;
+            private readonly System.Windows.Forms.ListView _listView; // Добавляем поле для хранения ссылки на listView1
+            private Form1 form1;
+            private System.Windows.Forms.ListView listView1;
+
+            public PersonPresenter(IPersonView view, System.Windows.Forms.ListView listView) // Добавляем параметр для передачи ссылки на listView1
+            {
+                _view = view;
+                _people = new List<Person>();
+                _listView = listView; // Сохраняем ссылку на listView1
+
+                _view.AddPerson += AddPerson;
+                _view.SearchPerson += SearchPerson;
+            }
+
+            public PersonPresenter(Form1 form1, System.Windows.Forms.ListView listView1)
+            {
+                this.form1 = form1;
+                this.listView1 = listView1;
+            }
+
+            private void AddPerson(object sender, EventArgs e)
+            {
+                var person = new Person
+                {
+                    Name = _view.Name,
+                    Age = _view.Age
+                };
+
+                _people.Add(person);
+            }
+            private void SearchPerson(object sender, EventArgs e)
+            {
+                string searchName = _view.Name;
+                var result = _people.FirstOrDefault(person => person.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase));
+
+                _listView.Items.Clear();
+                if (result != null) // Проверяем, что result не равен null
+                {
+                    _listView.Items.Add(result);
+                }
+            }
+            private void SavePeopleToFile(List<Person> people, string filePath)
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8)) // Указываем кодировку UTF-8
+                    {
+                        foreach (Person person in people)
+                        {
+                            writer.WriteLine($"{person.Name},{person.Age}");
+                        }
+                    }
+
+                    MessageBox.Show("Данные успешно сохранены в файл.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при сохранении данных: {ex.Message}");
+                }
+            }
+
+            private void Save_Click(object sender, EventArgs e)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    SavePeopleToFile(_people, filePath);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog(); // Создаем объект класса OpenFileDialog
+                openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName; // Получаем путь к выбранному файлу
+
+                    listView1.Items.Clear();
+                    using (StreamReader reader = new StreamReader(filePath, Encoding.UTF8)) // Указываем кодировку UTF-8
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(',');
+                            if (parts.Length == 2)
+                            {
+                                string name = parts[0].Trim();
+                                int age;
+                                if (int.TryParse(parts[1].Trim(), out age))
+                                {
+                                    ListViewItem item = new ListViewItem(name);
+                                    item.SubItems.Add(age.ToString());
+                                    listView1.Items.Add(item);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) // Обрабатываем исключения
+            {
+                MessageBox.Show($"Произошла ошибка при загрузке данных: {ex.Message}");
+            }
+        }
+    }
+}
